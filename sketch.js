3867eef028715780328d797dfb88d7add738afec
Sankaran21 <69213552+Sankaran21@users.noreply.github.com>
2020-09-09 11:37:31 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,48 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+
+
+var ground,paper, dustbin;
+
+
+function setup() {
+	createCanvas(1600, 700);
+
+    
+	engine = Engine.create();
+	world = engine.world;
+	
+	ground=new Ground(800,680,1600,20);
+	paper=new Paper(200,450,40);
+	dustbin= new Dustbin(1200,650);
+	//Create the Bodies Here.
+
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+
+  paper.display();
+  ground.display();
+  dustbin.display();
+  drawSprites();
+ 
+}
+
+function keyPressed(){
+	if (keyCode === UP_ARROW) {
+
+    	Matter.Body.applyForce(paper.body,paper.body.position,{x:85,y:-85});
+    
+  	}
+
+}
+
+
